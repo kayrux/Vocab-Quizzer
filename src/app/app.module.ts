26b2454f9719835cb12ece88df2c36ce863b592f
Surya Kusjanto <skusjanto@outlook.com>
2023-08-16 19:02:14 -0600

    Added theme and basic routing. Set up angular material.

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,11 +1,19 @@
+import { MatButtonModule } from '@angular/material/button';
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+import { SharedAngularMaterialModule } from './shared/shared-angular-material/shared-angular-material.module';
 
 @NgModule({
   declarations: [AppComponent],
-  imports: [BrowserModule, AppRoutingModule],
+  imports: [
+    BrowserModule,
+    AppRoutingModule,
+    SharedAngularMaterialModule,
+    BrowserAnimationsModule,
+  ],
   providers: [],
   bootstrap: [AppComponent],
 })
---
+++ src/app/shared/shared-angular-material/shared-angular-material.module.ts
@@ -0,0 +1,95 @@
+import { DragDropModule } from '@angular/cdk/drag-drop';
+import { CommonModule } from '@angular/common';
+import { NgModule } from '@angular/core';
+import { MatButtonModule } from '@angular/material/button';
+import { MatButtonToggleModule } from '@angular/material/button-toggle';
+import { MatCardModule } from '@angular/material/card';
+import { MatCheckboxModule } from '@angular/material/checkbox';
+import { MatChipsModule } from '@angular/material/chips';
+import { MatNativeDateModule } from '@angular/material/core';
+import { MatDatepickerModule } from '@angular/material/datepicker';
+import { MatDialogModule } from '@angular/material/dialog';
+import { MatFormFieldModule } from '@angular/material/form-field';
+import { MatIconModule } from '@angular/material/icon';
+import { MatInputModule } from '@angular/material/input';
+import { MatListModule } from '@angular/material/list';
+import { MatMenuModule } from '@angular/material/menu';
+import { MatPaginatorModule } from '@angular/material/paginator';
+import { MatProgressBarModule } from '@angular/material/progress-bar';
+import { MatProgressSpinnerModule } from '@angular/material/progress-spinner';
+import { MatRadioModule } from '@angular/material/radio';
+import { MatSelectModule } from '@angular/material/select';
+import { MatSlideToggleModule } from '@angular/material/slide-toggle';
+import { MatSliderModule } from '@angular/material/slider';
+import { MatSnackBarModule } from '@angular/material/snack-bar';
+import { MatStepperModule } from '@angular/material/stepper';
+import { MatTableModule } from '@angular/material/table';
+import { MatTabsModule } from '@angular/material/tabs';
+import { MatToolbarModule } from '@angular/material/toolbar';
+import { MatTooltipModule } from '@angular/material/tooltip';
+
+@NgModule({
+  declarations: [],
+  imports: [
+    CommonModule,
+    MatInputModule,
+    MatButtonModule,
+    MatButtonToggleModule,
+    MatSliderModule,
+    MatStepperModule,
+    MatIconModule,
+    MatCardModule,
+    MatCheckboxModule,
+    MatListModule,
+    MatDialogModule,
+    MatSnackBarModule,
+    MatDatepickerModule,
+    MatNativeDateModule,
+    MatTooltipModule,
+    MatFormFieldModule,
+    MatRadioModule,
+    MatProgressBarModule,
+    MatProgressSpinnerModule,
+    MatMenuModule,
+    MatToolbarModule,
+    MatChipsModule,
+    MatTabsModule,
+    MatTableModule,
+    MatSelectModule,
+    MatSlideToggleModule,
+    MatPaginatorModule,
+    DragDropModule,
+    MatListModule,
+  ],
+  exports: [
+    MatInputModule,
+    MatButtonModule,
+    MatButtonToggleModule,
+    MatSliderModule,
+    MatStepperModule,
+    MatIconModule,
+    MatCardModule,
+    MatCheckboxModule,
+    MatListModule,
+    MatDialogModule,
+    MatSnackBarModule,
+    MatDatepickerModule,
+    MatNativeDateModule,
+    MatTooltipModule,
+    MatFormFieldModule,
+    MatRadioModule,
+    MatProgressBarModule,
+    MatProgressSpinnerModule,
+    MatMenuModule,
+    MatToolbarModule,
+    MatChipsModule,
+    MatTabsModule,
+    MatTableModule,
+    MatSelectModule,
+    MatSlideToggleModule,
+    MatPaginatorModule,
+    DragDropModule,
+    MatListModule,
+  ],
+})
+export class SharedAngularMaterialModule {}
