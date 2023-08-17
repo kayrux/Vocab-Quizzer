20d125ad0c4fe5f0574e79246295b8b1ad1b659c
Surya Kusjanto <skusjanto@outlook.com>
2023-08-16 20:07:23 -0600

    Minor changes

--- src/app/quiz/feature/quiz-shell/quiz-shell.module.ts
+++ src/app/quiz/feature/quiz-shell/quiz-shell.module.ts
@@ -1,9 +1,10 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
+import { QuizComponent } from '../quiz.component';
 import { QuizShellRoutingModule } from './quiz-shell-routing.module';
 
 @NgModule({
-  declarations: [],
+  declarations: [QuizComponent],
   imports: [CommonModule, QuizShellRoutingModule],
 })
 export class QuizShellModule {}
