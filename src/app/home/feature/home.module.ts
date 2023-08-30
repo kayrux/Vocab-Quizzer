d0f22fbc49f613343a73aa2bb1c38a73c77901f8
unknown <ljj905071486@gmail.com>
2023-08-30 00:53:02 -0600

    added search vocab feature

--- src/app/home/feature/home.module.ts
+++ src/app/home/feature/home.module.ts
@@ -2,9 +2,10 @@ import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { HomePageRoutingModule } from './home-routing.module';
 import { HomeComponent } from './home.component';
+import { SharedAngularMaterialModule } from 'src/app/shared/shared-angular-material/shared-angular-material.module';
 
 @NgModule({
   declarations: [HomeComponent],
-  imports: [CommonModule, HomePageRoutingModule],
+  imports: [CommonModule, HomePageRoutingModule, SharedAngularMaterialModule],
 })
 export class HomeModule {}
