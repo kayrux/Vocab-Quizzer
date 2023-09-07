30fd0e10dd472184b0c10a6b51e644af7e313a6b
Surya Kusjanto <skusjanto@outlook.com>
2023-09-06 21:27:31 -0600

    Update default vocab lists. Quiz now supports articles. Improved question generation

--- src/app/quizzes/ui/quiz-summary/quiz-summary.component.ts
+++ src/app/quizzes/ui/quiz-summary/quiz-summary.component.ts
@@ -1,4 +1,6 @@
+import { QuizService } from 'src/app/quizzes/data-access/quiz.service';
 import { Component, Input } from '@angular/core';
+import { Subject } from 'rxjs';
 @Component({
   selector: 'app-quiz-summary',
   templateUrl: './quiz-summary.component.html',
@@ -7,4 +9,15 @@ import { Component, Input } from '@angular/core';
 export class QuizSummaryComponent {
   @Input() totalQuestions: number = 1;
   @Input() numCorrect: number = 0;
+  @Input() $onResetQuiz!: Subject<void>;
+
+  constructor(public quizService: QuizService) {}
+  public tryAgain() {
+    if (!this.quizService.quiz) {
+      console.log('Error! No quiz found!');
+      return;
+    }
+    this.quizService.newQuiz(this.quizService.quiz.type);
+    this.$onResetQuiz.next();
+  }
 }
